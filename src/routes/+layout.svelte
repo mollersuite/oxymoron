8e671a1084e2f37b4dde8cf57435ae95db6862a1
Jack <29169102+Jack5079@users.noreply.github.com>
2023-01-01 17:54:39 -0500

    refactor: :fire: fuck it we're doing git last

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,23 +28,10 @@
 			</svg>
 			<span>Etcetera</span>
 		</a>
-		/
+		<!-- /
 		<a href="https://luau.ml">
-			<svg
-				height="20"
-				viewBox="0 0 15 15"
-				fill="#ff4539"
-				style="background:white;padding:5px"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path d="m 12,11 v 3 h 3 v -3 z" />
-				<path d="m 8,11 v 3 h 3 v -3 z" />
-				<path d="m 0,11 v 1.5 L 1.5,14 H 7 v -3 z" />
-				<path d="m 9.6230469,7 v 3 H 12.623047 V 7 Z" />
-				<path d="M 3.5,0 2,1.5 V 3 10 H 7 9 V 6 h 3 V 3 h 2 V 1.5 L 12.5,0 Z" />
-			</svg>
-			<span>Luau.ml</span>
-		</a>
+			<span>Note</span>
+		</a> -->
 
 		<slot name="nav" />
 	</nav>
---
+++ src/routes/@etc/+page.svelte
@@ -0,0 +1,83 @@
+<main>
+	<header class="h-card">
+		<img
+			src="https://media.wetdry.world/accounts/headers/107/056/519/414/414/906/original/70fe7d75d3eb5dcb.png"
+			class="banner"
+			alt="Etcetera's banner"
+		/>
+		<div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 173.8 275.72"
+				height="50"
+				style="background: white;padding:5px;aspect-ratio: 1/1;border-radius: 5px;"
+				class="pfp"
+			>
+				<g transform="translate(-14.71 -21.92)" fill="black">
+					<circle cx="51.84" cy="48.35" r="21.6" />
+					<circle cx="49.33" class="middle" cy="139.79" r="34.62" />
+					<circle cx="134.49" cy="152.6" r="21.6" class="middle" />
+					<circle cx="141.87" cy="68.56" r="46.65" />
+					<circle cx="117.39" cy="241.5" r="56.14" class="bottom" />
+				</g>
+			</svg>
+			<h1 class="p-name">Etcetera</h1>
+			<small>@etc@cetera.uk</small>
+			<p class="p-note">Software of dubious quality.</p>
+			<dl class="stats">
+				<dt>Followers</dt>
+				<dd>10</dd>
+				<!-- <dt>Following</dt>
+				<dd>0</dd> -->
+				<dt>Posts</dt>
+				<dd>0</dd>
+			</dl>
+			<dl class="meta">
+				<dt>Developer blog (technical info about our software)</dt>
+				<dd><a rel="me" href="https://jack.cetera.uk">https://jack.cetera.uk</a></dd>
+				<dt>Website</dt>
+				<dd><a rel="me" class="verified" href="https://cetera.uk">https://cetera.uk</a></dd>
+				<dt>Free as in</dt>
+				<dd>freedom</dd>
+			</dl>
+		</div>
+	</header>
+</main>
+
+<style>
+	h1 {
+		font-size: 1.5em;
+		margin: 0;
+	}
+	.banner {
+		width: 100%;
+	}
+	.pfp {
+		margin-top: -30px;
+	}
+	header {
+		width: 300px;
+		display: block;
+	}
+	header div {
+		margin-left: 10px;
+	}
+	.stats {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+	.stats dt {
+		font-weight: bold;
+	}
+	.stats dd {
+		font-family: monospace;
+	}
+	.meta dt {
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+	.verified {
+		color: #6acb5d;
+	}
+</style>
