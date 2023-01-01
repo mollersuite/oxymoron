3edf9019740c74b402c7b6f62d1be6082831522f
Jack <29169102+Jack5079@users.noreply.github.com>
2023-01-01 17:55:54 -0500

    refactor: :lipstick: redesign header now that it's called oxymoron

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <header>
 	<nav>
-		<a href="/">
+		<a href="/" class="root" title="Oxymoron">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 173.8 275.72" height="20">
-				<g transform="translate(-14.71 -21.92)" fill="white">
+				<g transform="translate(-14.71 -21.92)" fill="#6acb5d">
 					<circle cx="51.84" cy="48.35" r="21.6" />
 					<circle cx="49.33" class="middle" cy="139.79" r="34.62" />
 					<circle cx="134.49" cy="152.6" r="21.6" class="middle" />
@@ -44,9 +44,11 @@
 		margin: 0;
 		min-height: 100vh;
 		display: grid;
+		font-family: sans-serif;
 		grid-template:
 			'header header' 50px
-			'sidebar main' 1fr / 200px 1fr;
+			'main main' 1fr;
+		/* 'sidebar main' 1fr / 200px 1fr; */
 	}
 	@media (max-width: 600px) {
 		:global(body) {
@@ -54,10 +56,16 @@
 		}
 	}
 	header {
-		background: black;
+		background: rgba(0, 0, 0, 0.5);
+		border-bottom-left-radius: 1ch;
+		border-bottom-right-radius: 1ch;
+		max-width: 1280px;
+		width: 100vw;
+		margin: 0 auto;
 		display: flex;
 		justify-content: stretch;
 		padding-inline: 1ch;
+		box-sizing: border-box;
 		align-items: center;
 		gap: 1ch;
 		flex-direction: row;
@@ -80,6 +88,7 @@
 		color: white;
 		text-decoration: none;
 		border-radius: 1ch;
+		transition: background 0.2s;
 	}
 	nav a:hover {
 		background: rgba(255, 255, 255, 0.1);
@@ -87,5 +96,11 @@
 	nav > a > svg {
 		aspect-ratio: 1 / 1;
 		border-radius: 5px;
+	}
+	.root:hover svg g {
+		transition: fill 0.2s;
+	}
+	.root:hover svg g {
+		fill: white;
 	}
 </style>
